bcbe32649758b16b219a3952aaea6c21c80e00fa
rohan12114 <122142354+rohan12114@users.noreply.github.com>
2023-06-26 14:42:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
-    //Write your code here
+// script.js
 
+function getFormValue() {
+  // Get the input values from the form
+  var firstName = document.getElementById("Rishabh").value;
+  var lastName = document.getElementById("Pant").value;
+
+  // Alert the first and last name
+  alert("First Name: " + firstName + "\nLast Name: " + lastName);
 }
+
